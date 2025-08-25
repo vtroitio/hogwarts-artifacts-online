14996a8925de8ee5f3f0a508f54248960bd15ecc
vtroitino-uade <valentinbtroitino@gmail.com>
2025-08-24 23:30:27 -0300

    primer commit

---
+++ src/main/java/com/valik/hogwarts_artifacts_online/HogwartsArtifactsOnlineApplication.java
@@ -0,0 +1,13 @@
+package com.valik.hogwarts_artifacts_online;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HogwartsArtifactsOnlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HogwartsArtifactsOnlineApplication.class, args);
+	}
+
+}
